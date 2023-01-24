108f90254ce3f10b307066c5484e6ad88ee42623
tatertot365 <tate.gillespie@gmail.com>
2023-01-24 11:31:01 -0700

    initial commit

---
+++ Mission03/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Mission3;
+
+namespace Mission03
+{
+    class Program
+    {
+        static bool SpotsFilled(char[] gameBoard)
+        {
+            bool NineSpotsFill = true;
+
+            for (int i = 0; i < gameBoard.Length; i++)
+            {
+                if (gameBoard[i] == '_')
+                {
+                    NineSpotsFill = false;
+                }
+            }
+
+            return NineSpotsFill;
+        }
+
+        static int UserInputVal(char[] gameBoard, int UserInput)
+        { 
+            while( UserInput > 8 || UserInput < 0)
+            {
+                Console.WriteLine("That the number is not between 0 and 8, try again.");
+                UserInput = int.Parse(Console.ReadLine());
+
+            }
+
+            while (gameBoard[UserInput] != '_')
+            {
+                Console.WriteLine("That spot is already taken, try again.");
+                UserInput = int.Parse(Console.ReadLine());
+            }
+
+            return UserInput;
+        }
+
+        static void Main(string[] args)
+        {
+            // Welcome the users to the game
+            Console.WriteLine("Welcome to Tic-Tac-Toe!");
+            Console.WriteLine("You may pick a position 0 through 8 to place an X or O");
+            Console.WriteLine("Positions start in the top-left corner, and move from left to right, down to the bottom-right corner.");
+
+            // The gameboard is initialized 
+            char[] gameBoard = { '_', '_', '_', '_', '_', '_', '_', '_', '_'};
+
+            // Empty gameboard is outputted to the console
+            Console.WriteLine("\t | | ");
+            Console.WriteLine("\t- - -");
+            Console.WriteLine("\t | | ");
+            Console.WriteLine("\t- - -");
+            Console.WriteLine("\t | |");
+
+            // Characters
+            char O = 'O';
+            char X = 'X';
+
+            // Checks which player it is
+            bool Player1 = true;
+
+            while (SpotsFilled(gameBoard) == false)
+            {
+                if (Player1)
+                {
+                    Console.WriteLine("Please select your position player 1: ");
+
+                    int Player1Position = int.Parse(Console.ReadLine());
+                    int UserInput = UserInputVal(gameBoard, Player1Position);
+
+                    Player1 = false;
+                    gameBoard[UserInput] = O;
+                }
+
+                else if (Player1 == false)
+                {
+                    Console.WriteLine("Please select your position player 2: ");
+
+                    int Player2Position = int.Parse(Console.ReadLine());
+                    int UserInput = UserInputVal(gameBoard, Player2Position);
+
+                    Player1 = true;
+                    gameBoard[UserInput] = X;
+                }
+
+                /*foreach (char ele in gameBoard)
+                {
+                    Console.WriteLine(ele);
+                }*/
+
+                Supporting sp = new Supporting();
+                sp.printBoard(gameBoard);
+                Console.WriteLine(sp.checkWinner(gameBoard));
+
+            }
+
+
+
+
+
+        }
+    }
+}
+
---
+++ Mission03/Supporting.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Mission3
+{
+    class Supporting
+    {
+        public void printBoard(char[] board)
+        {
+            Console.Write("\t" + board[0] + " | " + board[1] + " | " + board[2] + "\n");
+            Console.Write("\t" + "----------\n");
+            Console.Write("\t" + board[3] + " | " + board[4] + " | " + board[5] + "\n");
+            Console.Write("\t" + "----------\n");
+            Console.Write("\t" + board[6] + " | " + board[7] + " | " + board[8] + "\n");
+        }
+        public string checkWinner(char[] board)
+        {
+            string output = "No winner yet, keep playing!";
+            //ROW options for winning
+            //First row
+            if ((board[0] == board[1]) && (board[1] == board[2]) && board[0] != '_')
+            {
+                output = board[0] + " is the winner";
+            }
+            //Second row
+            else if ((board[3] == board[4]) && (board[4] == board[5]) && board[3] != '_')
+            {
+                output = board[3] + " is the winner!";
+            }
+            //Third row
+            else if ((board[6] == board[7]) && (board[7] == board[8]) && board[6] != '_')
+            {
+                output = board[6] + " is the winner";
+            }
+            //COLUMNS
+            //First column
+            else if ((board[0] == board[3]) && (board[3] == board[6]) && board[0] != '_')
+            {
+                output = board[0] + " is the winner";
+            }
+            //Second column
+            else if ((board[1] == board[4]) && (board[4] == board[7]) && board[1] != '_')
+            {
+                output = board[1] + " is the winner!";
+            }
+            //Third column
+            else if ((board[2] == board[5]) && (board[5] == board[8]) && board[2] != '_')
+            {
+                output = board[2] + " is the winner";
+            }
+            //DIAGNALS
+            //back diagnal
+            else if ((board[0] == board[4]) && (board[4] == board[8]) && board[0] != '_')
+            {
+                output = board[0] + " is the winner!";
+            }
+            //forward diagnal
+            else if ((board[2] == board[4]) && (board[4] == board[6]) && board[2] != '_')
+            {
+                output = board[2] + " is the winner";
+            }
+            return output;
+        }
+    }
+}
+
